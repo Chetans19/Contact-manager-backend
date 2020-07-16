06834496085e4ed4fb4154a2f78a58ec27165698
champ <itschetanshinde@gmail.com>
2020-07-16 20:26:41 +0530

    Update contact added

--- routes/contacts.js
+++ routes/contacts.js
@@ -87,15 +87,34 @@ router.put('/:id', auth, async (req, res) => {
             res.json(contact);
 
     } catch(err) {
-
+        console.error(er.message);
+        res.status(500).send('Server Error');
     }
 });
-
+ 
 // @route DELETE/api/contacts/:id
 // @desc Delete contact
 // @access Private
-router.delete('/:id', (req, res) => {
-    res.send('delete contact')
+router.delete('/:id', auth, async (req, res) => {
+
+    try {
+        let contact = await Contact.findById(req.params.id);
+
+        if(!contact) return res.status(404).json({ msg: 'contact not found'});
+
+        // Make sure user owns contact
+        if(contact.user.toString() !== req.user.id) {
+            return res.status(401).json({ msg: 'Not authorized'});
+        }
+
+        await Contact.findByIdAndRemove(req.params.id);
+
+        res.json({ msg: 'Contact removed' });
+
+    } catch(err) {
+        console.error(er.message);
+        res.status(500).send('Server Error');
+    }
 });
 
 module.exports = router;
